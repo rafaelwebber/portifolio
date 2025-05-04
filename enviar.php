8a48d3cf190db3f91627679ee8b0cf9af0a9b148
rafaelwebber <rafael.webber09@gmail.com>
2025-05-03 23:12:44 -0300

    colocando site no ar

--- enviar.php
+++ enviar.php
@@ -1,20 +1,26 @@
 <?php
+    // Sanitização de dados
+    $nome = htmlspecialchars($_POST['nome']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $telefone = htmlspecialchars($_POST['telefone']);
+    $mensagem = htmlspecialchars($_POST['mensagem']);
 
-    $nome = addslashes($_POST ['nome']);
-    $email = addslashes($_POST ['email']);
-    $telefone = addslashes($_POST ['telefone']);
-    $mensagem= addslashes($_POST ['mensagem']);
-
+    // Configuração do e-mail
     $para = "rafael.lumertz_webber@hotmail.com.br";
     $assunto = "Coleta de dados";
 
+    // Corpo do e-mail
     $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
 
-    $cabeca = "From: rafael.webber09@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+    // Cabeçalhos do e-mail
+    $cabeca = "From: rafael.webber09@gmail.com"."\n".
+              "Reply-to: ".$email."\n".
+              "X-Mailer: PHP/".phpversion();
 
-    if(mail($para,$assunto,$corpo,$cabeca,$mensagem)){
-        echo("E-mail enviado com sucesso!");
-    }else{
-        echo("Houve um erro ao enviar o e-mail!");
+    // Envio do e-mail
+    if(mail($para, $assunto, $corpo, $cabeca)){
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Houve um erro ao enviar o e-mail!";
     }
 ?>
