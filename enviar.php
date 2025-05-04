872229fb96ec5cf52732e04f8fb0c488bc5234b2
rafaelwebber <rafael.webber09@gmail.com>
2025-05-03 22:50:26 -0300

    arrumando erro

--- enviar.php
+++ enviar.php
@@ -3,15 +3,16 @@
     $nome = addslashes($_POST ['nome']);
     $email = addslashes($_POST ['email']);
     $telefone = addslashes($_POST ['telefone']);
+    $mensagem= addslashes($_POST ['mensagem']);
 
     $para = "rafael.lumertz_webber@hotmail.com.br";
     $assunto = "Coleta de dados";
 
-    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: ".$telefone;
+    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
 
-    $cabeca = "From: rafael.webber09@gmail.com". "\n"."Reply-to: ".$email. "\n"."X=Mailer:PHP/".phpversion();
+    $cabeca = "From: rafael.webber09@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
-    if(mail($para,$assunto,$corpo,$cabeca)){
+    if(mail($para,$assunto,$corpo,$cabeca,$mensagem)){
         echo("E-mail enviado com sucesso!");
     }else{
         echo("Houve um erro ao enviar o e-mail!");
